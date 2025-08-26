b3f4084ade793b6157e5aca9e3bdb26b0b9ca897
Nguyen Duc <68nguyenhuuduc@gmail.com>
2025-08-26 21:07:34 +0700

    Be quay

---
+++ AimTuret.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class AimTuret : MonoBehaviour
+{
+    public float turretRationSpeed = 100f;// tốc độ xoay của súng
+
+    public void Aim(Vector2 inputPointerPosition)
+    {
+        var turretDirection = (Vector3)inputPointerPosition - transform.position;
+        var desiredAngle = Mathf.Atan2(turretDirection.y, turretDirection.x) * Mathf.Rad2Deg;
+        var rotationStep = turretRationSpeed * Time.deltaTime;
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, 0, desiredAngle), rotationStep);
+    }
+}
